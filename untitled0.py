5fe488329df61c18b0059adb3ace1b711a3a338f
imRishabhGupta <rishabhgpt86@gmail.com>
2017-03-26 15:57:02 +0530

    connecting client

--- untitled0.py
+++ untitled0.py
@@ -7,28 +7,61 @@ Created on Sun Mar 26 04:05:27 2017
 #18a7b385ee3433d4d37492e4ce23d45a
 import requests
 import json
+import re
+import difflib
+import json
 import numpy as np
 from nltk import tokenize
 import http.client, urllib.request, urllib.parse, urllib.error, base64
 
+#save the string from API call here
+text = 'best red sauce pasta in medium budget'
 city="delhi ncr"
+'''
+m = re.search('best (.+?) under', text)
+if m:
+    word = m.group(1)
+
+with open('cuisine.json') as json_file:
+    data = json.load(json_file)
+    for p in data['dishes']:
+        seq=difflib.SequenceMatcher(word=word.lower(), p['dish_name']=p['dish_name'].lower())
+        if (seq.ratio>0.1):
+            cuisine = p['cuisine']
+            break
+
+n = re.search('under (.+?) budget', text)
+if m:
+    budget = m.group(1)
+
+if(budget == 'low'):
+    price_range = 2
+elif(budget == 'medium'):
+    price_range = 3
+else:
+    price_range = 4
+
+#low=2 medium =3 high =4 (price_range)
+'''
+#city="delhi ncr"
 word="Fried Rice"
+cuisine="Chinese"
 url = "https://developers.zomato.com/api/v2.1/cities?q="+city
 
 headers = {"Content-type": "application/json",
-           "user-key":"18a7b385ee3433d4d37492e4ce23d45a"}
+           "user-key":"78eba48da7a3f396db77cba175b53a9c"}
 r  = requests.get(url,headers=headers)
 
 jData = json.loads(r.content)
 print(jData)
 l=jData['location_suggestions']
-entity_id=0
+entity_id=-1
 for i in range(len(l)):
     if(l[i]['country_id']==1):
         entity_id=l[i]['id']
         
 print(entity_id)
-cuisine="Chinese"
+#cuisine="Chinese"
 url2 = "https://developers.zomato.com/api/v2.1/cuisines?city_id="+str(entity_id)
 r2  = requests.get(url2,headers=headers)
 jData2 = json.loads(r2.content)
@@ -51,6 +84,7 @@ j=0
 user_price=3
 
 user_restaurants_ids=[]
+user_restaurants_names=[]
 user_restaurants_ratings=[]
 while(True):
     url3="https://developers.zomato.com/api/v2.1/search?entity_id="+str(entity_id)+"&entity_type=city&start="+str(j)+"&cuisines="+str(cuisine_id)+"&sort=rating&order=desc"
@@ -62,7 +96,8 @@ while(True):
     for i in range(len(restaurants)):
         restaurant=restaurants[i]['restaurant']
         if(restaurant['price_range']<=user_price):
-            user_restaurants_ids.append(restaurant['R']['res_id'])
+            user_restaurants_ids.append(restaurant['id'])
+            user_restaurants_names.append(restaurant['name'])
             user_restaurants_ratings.append(restaurant['user_rating']['aggregate_rating'])    
     j=j+num_results
     #print(num)
@@ -99,7 +134,7 @@ for i in range(len(selected_restaurants)):
 print(reviews)
 
 def find_rating(restaurant_rating, review_rating):
-    return (restaurant_rating/5+review_rating)/2
+    return (float(restaurant_rating)/5+review_rating)/2
 
 def analyse_review(review):
     headers = {
@@ -114,7 +149,7 @@ def analyse_review(review):
                     {
                             "language": "en",
                             "id": "2",
-                                "text": review
+                            "text": review
                                 }
                     ]
             }
@@ -123,12 +158,12 @@ def analyse_review(review):
             conn.request("POST", "/text/analytics/v2.0/sentiment?%s" % params, str(body), headers)
             response = conn.getresponse()
             data = response.read()
-            j = json.loads(data.content)
+            j = json.loads(data)
             print(j['documents'][0]['score'])
             conn.close()
-            return ['documents'][0]['score']
+            return j['documents'][0]['score']
     except Exception as e:
-        print("[Errno {0}] {1}".format(e.errno, e.strerror))
+        return 0.3
     
 
 actual_ratings=[]
@@ -139,6 +174,8 @@ for i in range(len(reviews)):
     else:
         actual_ratings.append(find_rating(user_restaurants_ratings[i],analyse_review(reviews[i])))
         
-a=np.column_stack((user_restaurants_ids, actual_ratings))
+a=np.column_stack((user_restaurants_ids, actual_ratings,user_restaurants_names))
 a = sorted(a, key=lambda a_entry: a_entry[1]) 
-print(a)
+k=-1*min(10,len(a)) - 1
+print(a[:k:-1])
+#for i in range(min(len(a),10)):
