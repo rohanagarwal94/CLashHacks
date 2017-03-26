00fad4d5168df64096310c93c4f0e13aafb3a917
rohanagarwal94 <rohanagarwal94@yahoo.com.sg>
2017-03-26 12:03:38 +0530

    Recreate bot UI

---
+++ Clashhacks/app/src/main/java/com/example/rohanagarwal94/hackdtu/CancerDetectionActivity.java
@@ -0,0 +1,276 @@
+package com.example.rohanagarwal94.hackdtu;
+
+import android.Manifest;
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
+
+import com.firebase.client.Firebase;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.BufferedOutputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+@TargetApi(Build.VERSION_CODES.JELLY_BEAN)
+public class CancerDetectionActivity extends AppCompatActivity {
+    private static final int CAMERA_CAPTURE_REQUEST_CODE = 100;
+    private static final int GALLERY_REQUEST_CODE = 1234;
+    private static final int REQUEST_EXTERNAL_STORAGE = 1;
+    private int points = 0;
+    private Uri fileUri;
+    private Firebase mRef;
+    FirebaseStorage storage;
+    private ProgressDialog progress;
+    Bitmap image = null;
+    StorageReference storageRef;
+    private int _xDelta;
+    private int _yDelta;
+    private ViewGroup mRrootLayout;
+    private Uri finalUri;
+
+    private static String[] APP_PERMISSIONS = {
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+    };
+
+    ImageView imgContainer;
+
+//    @Bind(R.id.imageContLayout)
+    RelativeLayout imageContLayout;
+
+    @OnClick(R.id.gotoGallery)
+    void openGalleryImages() {
+        GetImagesGallery();
+    }
+
+    @OnClick(R.id.gotoCamera)
+    void openCameraImages() {
+        CaptureImageCamera();
+    }
+
+    @OnClick(R.id.upload)
+    void uploadToFireBase() {
+        uploadFile();
+    }
+
+
+    private void shareDrawImage() throws IOException {
+        imageContLayout.setDrawingCacheEnabled(true);
+        Bitmap bitmap = imageContLayout.getDrawingCache();
+        File file, f = null;
+        int filename = 1;
+        if (android.os.Environment.getExternalStorageState().equals(android.os.Environment.MEDIA_MOUNTED)) {
+            filename = filename + 1;
+            file = new File(android.os.Environment.getExternalStorageDirectory(), "Brand Ambassador");
+            if (!file.exists()) {
+                file.mkdirs();
+            }
+            f = new File(file.getAbsolutePath() + file.separator + "mukul" + filename + ".png");
+        }
+        FileOutputStream ostream = new FileOutputStream(f);
+        bitmap.compress(Bitmap.CompressFormat.PNG, 10, ostream);
+        ostream.close();
+        finalUri = Uri.fromFile(f);
+    }
+
+    private void CaptureImageCamera() {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent, CAMERA_CAPTURE_REQUEST_CODE);
+    }
+
+    private void GetImagesGallery() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select File"), GALLERY_REQUEST_CODE);
+    }
+
+    private void getCameraImageUri() {
+        new Thread() {
+            @Override
+            public void run() {
+                try {
+                    File file = new File(Environment.getExternalStorageDirectory(), "hashtag.jpg");
+                    OutputStream os = new BufferedOutputStream(new FileOutputStream(file));
+                    image.compress(Bitmap.CompressFormat.JPEG, 100, os);
+                    os.close();
+                    fileUri = Uri.fromFile(file);
+                } catch (Exception e) {
+                    Log.i("File Error", e.toString());
+                }
+            }
+        }.start();
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == CAMERA_CAPTURE_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                image = (Bitmap) data.getExtras().get("data");
+                imgContainer.setImageBitmap(image);
+                getCameraImageUri();
+            } else if (resultCode == RESULT_CANCELED) {
+                // user cancelled Image capture
+                Toast.makeText(getApplicationContext(),
+                        "User cancelled image capture", Toast.LENGTH_SHORT)
+                        .show();
+            } else {
+                // failed to capture image
+                Toast.makeText(getApplicationContext(),
+                        "Sorry! Failed to capture image", Toast.LENGTH_SHORT)
+                        .show();
+            }
+
+        }
+        if (requestCode == GALLERY_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                fileUri = data.getData();
+                try {
+                    image = MediaStore.Images.Media.getBitmap(getContentResolver(), fileUri);
+                    image = Bitmap.createScaledBitmap(image, 500, 500, false);
+                    imgContainer.setImageBitmap(image);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            } else if (resultCode == RESULT_CANCELED) {
+                // user cancelled Image capture
+                Toast.makeText(getApplicationContext(),
+                        "User cancelled image selection", Toast.LENGTH_SHORT)
+                        .show();
+            } else {
+                // failed to capture image
+                Toast.makeText(getApplicationContext(),
+                        "Sorry! Failed to open Gallery", Toast.LENGTH_SHORT)
+                        .show();
+            }
+        }
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.cancer_detection_activity);
+        ButterKnife.bind(this);
+        storage = FirebaseStorage.getInstance();
+        verifyStoragePermissions(this);
+        mRrootLayout = (ViewGroup) findViewById(R.id.imageContLayout);
+        imgContainer=(ImageView)findViewById(R.id.imgContainer);
+//        storageRef = storage.getReferenceFromUrl("gs://healthbot-e0d1e.appspot.com");
+
+    }
+
+
+    public static void verifyStoragePermissions(Activity activity) {
+        int permission = ActivityCompat.checkSelfPermission(activity, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            // We don't have permission so prompt the user
+            ActivityCompat.requestPermissions(
+                    activity,
+                    APP_PERMISSIONS,
+                    REQUEST_EXTERNAL_STORAGE
+            );
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    private void uploadFile() {
+//        if (fileUri != null) {
+//            //displaying a progress dialog while upload is going on
+//            final ProgressDialog progressDialog = new ProgressDialog(this);
+//            progressDialog.setTitle("Uploading");
+//            progressDialog.show();
+//
+//            StorageReference riversRef = storageRef.child(fileUri.getLastPathSegment());
+//            riversRef.putFile(fileUri)
+//                    .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+//                        @Override
+//                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+//                            //if the upload is successfull
+//                            //hiding the progress dialog
+//                            progressDialog.dismiss();
+//
+//                            //and displaying a success toast
+//                            Toast.makeText(getApplicationContext(), "File Uploaded ", Toast.LENGTH_LONG).show();
+//                        }
+//                    })
+//                    .addOnFailureListener(new OnFailureListener() {
+//                        @Override
+//                        public void onFailure(@NonNull Exception exception) {
+//                            //if the upload is not successfull
+//                            //hiding the progress dialog
+//                            progressDialog.dismiss();
+//
+//                            //and displaying error message
+//                            Toast.makeText(getApplicationContext(), exception.getMessage(), Toast.LENGTH_LONG).show();
+//                        }
+//                    })
+//                    .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+//                        @Override
+//                        public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
+//                            //calculating progress percentage
+//                            double progress = (100.0 * taskSnapshot.getBytesTransferred()) / taskSnapshot.getTotalByteCount();
+//
+//                            //displaying percentage in progress dialog
+//                            progressDialog.setMessage("Uploaded " + ((int) progress) + "%...");
+//                        }
+//                    });
+//        }
+        //if there is not any file
+        imgContainer.setDrawingCacheEnabled(true);
+        imgContainer.buildDrawingCache();
+        Bitmap bitmap = imgContainer.getDrawingCache();
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+        byte[] data = baos.toByteArray();
+
+        UploadTask uploadTask = storageRef.putBytes(data);
+        uploadTask.addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception exception) {
+                // Handle unsuccessful uploads
+            }
+        }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                // taskSnapshot.getMetadata() contains file metadata such as size, content-type, and download URL.
+                Uri downloadUrl = taskSnapshot.getDownloadUrl();
+            }
+        });
+
+    }
+
+}
---
+++ Clashhacks/app/src/main/java/com/example/rohanagarwal94/hackdtu/DiabetesActivity.java
@@ -0,0 +1,115 @@
+package com.example.rohanagarwal94.hackdtu;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by rohanagarwal94 on 11/2/17.
+ */
+public class DiabetesActivity extends AppCompatActivity {
+
+    EditText e1,e2,e3,e4,e5,e6,e7,e8;
+
+    Button b1;
+    private RequestQueue requestQueue;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.diabetes_layout);
+        e1=(EditText)findViewById(R.id.editText);
+        e2=(EditText)findViewById(R.id.editText2);
+        e3=(EditText)findViewById(R.id.editText3);
+        e4=(EditText)findViewById(R.id.editText4);
+        e5=(EditText)findViewById(R.id.editText5);
+        e6=(EditText)findViewById(R.id.editText6);
+        e7=(EditText)findViewById(R.id.editText7);
+        e8=(EditText)findViewById(R.id.editText8);
+
+        b1=(Button)findViewById(R.id.data);
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getEmotion();
+            }
+        });
+    }
+
+    void getEmotion(){
+        requestQueue = Volley.newRequestQueue(DiabetesActivity.this);
+        String url="https://codeslayers.herokuapp.com/detectDiabetes";
+//        HashMap<String, String> params = new HashMap<String, String>();
+//        params.put("text", "A suicide note or death note is a message left behind before a person has committed suicide, or who intends to commit suicide.  It is estimated that 25–30% of suicides are accompanied by a note. According to Gelder, Mayou and Geddes (2005) one in six leaves a suicide note. The content can be a plea for absolution or blaming family and friends for life's failings. However, incidence rates may depend on ethnicity, method of suicide, and cultural differences, and may reach rates as high as 50% in certain demographics.[1] A suicide message can be a written note, an audio message, or a video.");
+        JSONObject jsonObject=new JSONObject();
+        try {
+            jsonObject.put("1",e1.getText().toString());
+            jsonObject.put("2",e2.getText().toString());
+            jsonObject.put("3",e3.getText().toString());
+            jsonObject.put("4",e4.getText().toString());
+            jsonObject.put("5",e5.getText().toString());
+            jsonObject.put("6",e6.getText().toString());
+            jsonObject.put("7",e7.getText().toString());
+            jsonObject.put("8",e8.getText().toString());
+
+        }catch (JSONException e){
+            //Toast.makeText(getApplicationContext(), "Json exception", Toast.LENGTH_SHORT).show();
+        }
+        JsonObjectRequest jor = new JsonObjectRequest(url, jsonObject,
+                new com.android.volley.Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            if(response.getInt("ans") == 0)
+                            Toast.makeText(DiabetesActivity.this, "No diabetes", Toast.LENGTH_LONG).show();
+                            else
+                                Toast.makeText(DiabetesActivity.this, "You have diabetes", Toast.LENGTH_LONG).show();
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                        System.out.println(response.toString());
+
+
+
+                    }
+                },
+                new com.android.volley.Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(getApplicationContext(),"User already registered or Error in internet connection.",Toast.LENGTH_LONG).show();
+                        Log.e("Volley",error.toString());
+                    }
+                }
+        ){
+            @Override
+            protected Map<String,String> getParams(){
+                Map<String,String> params = new HashMap<String, String>();
+//                params.put("text", "A suicide note or death note is a message left behind before a person has committed suicide, or who intends to commit suicide.  It is estimated that 25–30% of suicides are accompanied by a note. According to Gelder, Mayou and Geddes (2005) one in six leaves a suicide note. The content can be a plea for absolution or blaming family and friends for life's failings. However, incidence rates may depend on ethnicity, method of suicide, and cultural differences, and may reach rates as high as 50% in certain demographics.[1] A suicide message can be a written note, an audio message, or a video.");
+                //  params.put("","");
+                return params;
+            }
+
+
+        };
+        requestQueue.add(jor);
+
+    }
+
+}
